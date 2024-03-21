9544ecaedb24228a3332e54e738cd6558416e158
V Harish <152043392+Lucifer516-sudoer@users.noreply.github.com>
2024-03-21 08:47:25 +0530

    Update new

---
+++ resources.py
@@ -0,0 +1,5 @@
+vdos = [
+"https://youtu.be/k0zKoTvngUY?si=MwBOkM4dCc0ByYbc",
+"https://youtu.be/HfACrKJ_Y2w?si=1-8USNxmivhxQcVi",
+"https://youtu.be/7gigNsz4Oe8?si=0BbrB4uG-7NNMNHd",
+"https://youtu.be/LfaMVlDaQ24?si=eaOKgMc0OvF5DVaO"]
